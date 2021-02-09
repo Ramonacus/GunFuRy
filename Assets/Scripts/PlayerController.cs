16e7b6a697e62a2f03d098c04e55185b6bdad429
Vanessa Badia <vanebadia@gmail.com>
2021-02-10 00:00:15 +0100

    Simple player controller and zombi chaser

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        direction = new Vector2(horizontalInput, verticalInput);
+        direction = Vector2.ClampMagnitude(direction, 1);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ZombiMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        direction = Vector2.ClampMagnitude(direction, 1);
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
