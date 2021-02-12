4e0c45eb9b5bec1462aba7fafbf23925700242e9
Ramonacus <ramonacus@gmail.com>
2021-02-12 19:27:15 +0100

    Attack animation

--- Assets/Scripts/ZombiMovement.cs
+++ Assets/Scripts/ZombiMovement.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class ZombiMovement : MonoBehaviour
 {
-    private GameObject player;
+    private enum State
+    {
+        Idle,
+        Walking,
+        Attacking
+    }
+    State state;
+
+    GameObject player;
+    Animator animator;
 
     Rigidbody2D enemyRb;
     public float speed = 2f;
@@ -13,36 +22,65 @@ public class ZombiMovement : MonoBehaviour
     void Awake()
     {
         player = GameObject.Find("Player");
+        state = State.Idle;
         enemyRb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         Vector2 directionToPlayer = player.transform.position - transform.position;
+
+
+        switch (state)
+        {
+            case State.Idle:
+            case State.Walking:
+                StandingUpdate(directionToPlayer);
+                break;
+            case State.Attacking:
+                break;
+        }
+    }
+
+    void StandingUpdate(Vector2 directionToPlayer)
+    {
+        // TODO replace with actual animations for moving left
+        transform.rotation = new Quaternion(0, directionToPlayer.x > 0 ? 0 : 180, 0, 0);
+
         if (directionToPlayer.magnitude > attackRange)
         {
+            animator.SetBool("Attacking", false);
             directionToPlayer = Vector2.ClampMagnitude(directionToPlayer, 1);
             enemyRb.MovePosition(GetPosition2D() + directionToPlayer * speed * Time.deltaTime);
         }
         else
         {
+            Attack(directionToPlayer.normalized);
+        }
+    }
 
-        }
+    void Attack(Vector2 direction)
+    {
+        state = State.Attacking;
+        animator.SetBool("Attacking", true);
+    }
 
-        // TODO replace with actual animations for moving left
-        if (directionToPlayer.x < 0)
-        {
-            transform.rotation = new Quaternion(0, 180, 0, 0);
-        }
-        else
-        {
-            transform.rotation = new Quaternion(0, 0, 0, 0);
-        }
+    public void EndAttack()
+    {
+        state = State.Idle;
+        animator.SetBool("Attacking", false);
     }
 
     Vector2 GetPosition2D()
     {
         return new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(GetPosition2D(), attackRange);
     }
 }
