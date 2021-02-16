260fc03094d536cf29ea79abad5652c3f4f5f7d1
Vanessa Badia <vanebadia@gmail.com>
2021-02-17 00:44:18 +0100

    Zombi attack

--- Scripts/Zombi.gd
+++ Scripts/Zombi.gd
@@ -5,6 +5,11 @@ var state = State.STANDING
 
 var speed = 50
 var direction = Vector2()
+
+var attack_range = 20
+var attack_cooldown = 1.4
+var attack_timer
+
 onready var player: KinematicBody2D = get_node("../Player")
 onready var sprite: AnimatedSprite = $AnimatedSprite
 
@@ -17,9 +22,31 @@ func _physics_process(delta):
 func _process(delta):
 	match state:
 		State.STANDING:	
-			process_standing()
+			process_standing(delta)
+		State.ATTACKING:
+			process_attacking(delta)
 
 
-func process_standing():
-	direction = (player.get_position() - get_position()).normalized()
+func process_standing(delta):
+	var vectorToPlayer = player.get_position() - get_position()
+	direction = vectorToPlayer.normalized()
 	sprite.set_flip_h(direction.x < 0)
+	
+	if vectorToPlayer.length() <= attack_range:
+		attack()
+	print(vectorToPlayer.length() )
+
+
+func process_attacking(delta):
+	attack_timer -= delta
+	if attack_timer <= 0:
+		state = State.STANDING
+		sprite.animation = "Zombi walk E"
+
+
+func attack():
+	direction = Vector2()
+	state = State.ATTACKING
+	sprite.animation = "Zombi attack E"
+	attack_timer = attack_cooldown
+	
