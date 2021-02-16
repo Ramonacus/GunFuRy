5c9f4a9be5a296260b752ad62c349e9fe66fac5b
Vanessa Badia <vanebadia@gmail.com>
2021-02-16 23:55:32 +0100

    Refactor Zombi

--- Scripts/Zombi.gd
+++ Scripts/Zombi.gd
@@ -1,19 +1,25 @@
 extends KinematicBody2D
 
+enum State {STANDING, ATTACKING}
+var state = State.STANDING
+
 var speed = 50
 var direction = Vector2()
 onready var player: KinematicBody2D = get_node("../Player")
 onready var sprite: AnimatedSprite = $AnimatedSprite
 
-func calculate_direction():
-	direction = (player.get_position() - get_position()).normalized()
-	sprite.set_flip_h(direction.x < 0)
 
 func _physics_process(delta):
-	calculate_direction()
 	move_and_collide(speed * direction * delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	match state:
+		State.STANDING:	
+			process_standing()
+
+
+func process_standing():
+	direction = (player.get_position() - get_position()).normalized()
+	sprite.set_flip_h(direction.x < 0)
