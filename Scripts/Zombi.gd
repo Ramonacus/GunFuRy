495acfed35ff4a7c9118ac7eb5427ab075b4164b
Ramonacus <ramonacus@gmail.com>
2021-02-18 19:21:25 +0100

    Zombi states

--- Scripts/Zombi.gd
+++ Scripts/Zombi.gd
@@ -1,7 +1,10 @@
-extends KinematicBody2D
-
-enum State {STANDING, ATTACKING}
-var state = State.STANDING
+const State = {
+	IDLE = "idle",
+	WALKING = "walking",
+	ATTACKING = "attacking"
+}
+var state = State.IDLE
+var previous_state = State.IDLE
 
 # Animations enum
 const Animations = {
@@ -18,54 +21,53 @@ onready var player: KinematicBody2D = get_node("../Player")
 
 
 func _ready():
-	start_standing()
-
+	state = State.WALKING
 
 func _physics_process(delta):
 	move_and_collide(speed * direction * delta)
 
-
 func _process(delta):
-	match state:
-		State.STANDING:	
-			process_standing(delta)
-		State.ATTACKING:
-			process_attacking(delta)
+	if state != previous_state:
+		change_state()
+	if has_method("process_" + state):
+		call("process_" + state, delta)
+
+func change_state(new_state = null):
+	if (new_state):
+		state = new_state
+	if state == previous_state:
+		print_debug("Changing to the same state")
+	
+	if has_method("start_" + state):
+		call("start_" + state)
+	if has_method("stop_" + previous_state):
+		call("stop_" + previous_state)
+	previous_state = state
 
 
-# Standing state
-func start_standing():
-	state = State.STANDING
+# WALKING state
+func start_walking():
 	$AnimationPlayer.play(Animations.WALK)
 
-
-func process_standing(delta):
+func process_walking(delta):
 	var vector_to_player = player.get_position() - get_position()
 	$Sprite.set_flip_h(vector_to_player.x < 0)	
 	
 	if vector_to_player.length() <= attack_range:
-		start_attack(vector_to_player)
+		state = State.ATTACKING
 	else:
 		direction = vector_to_player.normalized()
 
 
 # Attack state
-func start_attack(vector_to_player):
+func start_attacking(vector_to_player):
 	direction = Vector2()
-	state = State.ATTACKING
 	$AnimationPlayer.play(Animations.ATTACK)
 	$Hitbox.rotation = Vector2.RIGHT.angle_to(vector_to_player)
 
-
-func process_attacking(delta):
-	pass
-
-
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name != Animations.WALK:
-		start_standing()
-
-
+		state = State.WALKING
 
 func _on_Hitbox_area_entered(area):
 	area.find_parent("Player").take_damage()
